e7ecbf255daefaa4f1c50bd6849531a260198a89
Yasharel Saiedian <yasharelsdn@gmail.com>
2020-03-28 21:43:19 -0400

    Setup server event handling and Redux store

--- client/src/index.js
+++ client/src/index.js
@@ -12,9 +12,29 @@ const sockjsURL = 'http://localhost/sockjs';
 
 const sock = new sockjsClient(sockjsURL);
 
+const store = createStore(rootReducer);
+
+sock.onopen = () => {
+  store.dispatch({type: "SOCKET_CONNECTED"});
+};
+
+sock.messageHandlers = {};
+
+sock.on = (event, fct) => {
+  sock.messageHandlers[event] = fct;
+}
+sock.onmessage = message => {
+  let data = JSON.parse(message.data);
+  let event = data.event;
+  if (!sock.messageHandlers[event]) return;
+  sock.messageHandlers[event](data.payload);
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App socket={sock}/>
+    <Provider store={store}>
+      <App socket={sock}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
