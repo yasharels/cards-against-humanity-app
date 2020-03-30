0fbe1522c7fffee4873c909e24c4944483024cd9
Yasharel Saiedian <yasharelsdn@gmail.com>
2020-03-29 21:43:10 -0400

    Create GameCard component

---
+++ client/src/components/GameCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class GameCard extends Component {
+  id =  this.props.game.id;
+  gamePoint = this.props.game.gamePoint;
+  users = this.props.game.users;
+  creator = this.props.game.creator;
+
+  render() {
+    return (
+      <React.Fragment>
+        <ul>
+          <li key={1}>Gamepoint: {this.gamePoint}</li>
+          <li key={2}>Users: {this.users}</li>
+        </ul>
+        <Link to={`/game/${this.id}`}><button>Join this game.</button></Link>
+      </React.Fragment>
+    );
+  }
+}
+
+export default GameCard;
