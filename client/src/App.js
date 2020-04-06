2fa9912f7814cf33e9c2880de51da386552c4372
Yasharel Saiedian <yasharelsdn@gmail.com>
2020-04-06 00:06:13 -0400

    Edit Lobby and Game component routing into App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Lobby from './components/Lobby';
+import GameRoom from './components/GameRoom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <Route
+          exact path="/"
+          render={ props => {
+            if (this.props.isLoading) return "im loading";
+            return  <Lobby {...props} socket={this.props.socket}/>
+          }}
+        />
+        <Route
+          path="/game/:id"
+          render={props => {
+            if (this.props.isLoading) return "im loading";
+            return <GameRoom {...props} socket={this.props.socket}/>
+          }}
+        />
+    </Router>
+  )
+}
 }
 
 export default App;
